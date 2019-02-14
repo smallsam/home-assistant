58e276dd188eb846479ec1b3f51fe24d90f48cda
Sam Richards <sam.richards@flightcentre.com.au>
2019-02-14 19:21:06 +1000

    Refactoring and checking in progress

--- homeassistant/components/climate/myair.py
+++ homeassistant/components/climate/myair.py
@@ -8,57 +8,91 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.components.nest import DATA_NEST
+import homeassistant.helpers.config_validation as cv
+
 from homeassistant.components.climate import (
     STATE_AUTO, STATE_COOL, STATE_HEAT, ClimateDevice,
     PLATFORM_SCHEMA, ATTR_TARGET_TEMP_HIGH, ATTR_TARGET_TEMP_LOW,
-    ATTR_TEMPERATURE)
+    ATTR_TEMPERATURE, SUPPORT_TARGET_TEMPERATURE, SUPPORT_OPERATION_MODE,
+    SUPPORT_FAN_MODE, SUPPORT_ON_OFF)
 from homeassistant.const import (
     TEMP_CELSIUS, TEMP_FAHRENHEIT,
     CONF_SCAN_INTERVAL, STATE_ON, STATE_OFF, STATE_UNKNOWN)
+import json
+
+DOMAIN = 'myair'
+
+CONF_IP = 'ip'
+CONF_PORT = 'port'
+
+STATE_DRY = 'dry'
+STATE_FAN_ONLY = 'fan'
+
+SUPPORT_FLAGS_UNIT = (SUPPORT_FAN_MODE | SUPPORT_OPERATION_MODE | SUPPORT_ON_OFF )
+SUPPORT_FLAGS_ZONE = ( SUPPORT_TARGET_TEMPERATURE | SUPPORT_ON_OFF )
+
 
 #DEPENDENCIES = ['pymyair']
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_SCAN_INTERVAL):
-        vol.All(vol.Coerce(int), vol.Range(min=1)),
+    vol.Required(CONF_IP): cv.string,
+    vol.Required(CONF_PORT, default=2025): cv.port,
 })
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     """Setup the MyAir Zoning"""
     if discovery_info is None:
         return
 
-    _LOGGER.debug("Setting up myair")
-
     temp_unit = hass.config.units.temperature_unit
+    
+    conf_ip = discovery_info.get(CONF_IP)
+    conf_port = discovery_info.get(CONF_PORT)
 
-    myairdevice = hass.data[DATA_MYAIR]
+    _LOGGER.info("got conf: %s" % ( conf_ip ) )
+    
+    # connect to myair in order to create 1 climate entity per AC unit controlled by the system
+    # often only one AC unit, but the controller is capable of controlling multiple
+    from pymyair.pymyair import MyAir
+    myair = MyAir(conf_ip,conf_port)
+    myair.update()
+    units = myair.system['aircons']
 
-    add_devices(
-        [MyAirZone(zoneid, myairdevice.myair)
-         for zoneid in myairdevice.zones()],
+    ac_units = [MyAirACUnit(conf_ip, conf_port, aircon=unit)
+         for unit in units]
+    add_entities(
+        ac_units,
+        True
+        )
+    
+    zones = []
+
+    for unit in ac_units:
+        unit.update()
+        for zone in unit.myair_system.zones:
+            zones += [MyAirZone(zone, unit)]
+
+    # For each AC unit, enumerate zones
+    add_entities(
+        zones,
         True
         )
 
-class MyAirZone(ClimateDevice):
-    """Representation of a MyAir Zone."""
+class MyAirACUnit(ClimateDevice):
+    """Representation of a MyAir Controller."""
 
-    def __init__(self, zone_id, myair_system):
-        """Initialize the thermostat."""
-        self.name = None
-        self.zone_id = zone_id
-        self.myair_system = myair_system
+    def __init__(self, ip, port, aircon):
+        from pymyair.pymyair import MyAir
+
+        self.myair_system = MyAir(ip,port,aircon=aircon)
         self._fan_list = ['low', 'medium', 'high', STATE_AUTO]
         self._operation_list = [STATE_OFF, STATE_HEAT, STATE_COOL, STATE_DRY, STATE_FAN_ONLY]
 
-        # feature of device
         self._has_fan = True
 
         # data attributes
-        self._name = None
         self._humidity = None
         self._target_temperature = None
         self._temperature = None
@@ -69,13 +103,69 @@ class MyAirZone(ClimateDevice):
         self._locked_temperature = None
 
     @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS_UNIT
+
+    @property
+    def current_operation(self):
+        """Return current operation ie. heat, cool, idle."""
+        if self._mode in [STATE_HEAT, STATE_COOL, STATE_DRY, STATE_FAN_ONLY, STATE_OFF]:
+            return self._mode
+        else:
+            return STATE_UNKNOWN
+
+    @property
+    def temperature_unit(self):
+        return self._temperature_scale
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._temperature
+    
+    def update(self):
+        """Cache value from MyAir"""
+        #zone_state = self.myair_system.getZone(self.zone_id)
+        #system_mode = self.myair_system.getMode()
+        #self._name = zone_state.get('name')
+        self.myair_system.update()
+        self._mode = self.myair_system.mode
+        #TODO myzone? self._target_temperature = 
+        #TODO myzone? self._temperature = self.myair_system.
+
+class MyAirZone(ClimateDevice):
+    """Representation of a MyAir Zone."""
+
+    def __init__(self, zone_id, myair_controller):
+        """Initialize the thermostat."""
+        self.zone_id = zone_id
+        self.myair_system = myair_controller.myair_system
+        self._name = ("Zone %s" % (zone_id))
+
+        # data attributes
+        self._humidity = None
+        self._target_temperature = None
+        self._temperature = None
+        self._temperature_scale = TEMP_CELSIUS
+        self._mode = None
+        self._fan = None
+        self._is_locked = None
+        self._locked_temperature = None
+
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS_ZONE
+           
+    @property
     def name(self):
         """Return the name of the zone, if any."""
         return self._name
 
     @property
     def temperature_unit(self):
-        """Return the unit of measurement."""
         return self._temperature_scale
 
     @property
@@ -136,14 +226,11 @@ class MyAirZone(ClimateDevice):
         """Turn fan on/off."""
         self.device.fan = fan.lower()
 
-
     def update(self):
-        """Cache value from MyAir"""
-        zone_state = self.myair_system.getZone(self.zone_id)
-        system_mode = self.myair_system.getMode()
-        self._name = zone_state.get('name')
-        self._temperature = zone_state.get('actualTemp')
-        self._mode = self.myair_system.get('setting')
-        self._target_temperature = zone_state.get('desiredTemp')
-        self.operation_mode = system_mode
+        self.myair_system.update()
+        #self._name = zone_state.get('name')
+        #self._mode = self.myair_system.mode  
+        #self._target_temperature = self.myair_system.zones[self.zoneid]['setTemp']
+        #self._temperature = self.myair_system.zones[self.zoneid]['actualTemp']
+
 
--- homeassistant/components/myair.py
+++ homeassistant/components/myair.py
@@ -17,70 +17,28 @@ from homeassistant.loader import get_component
 _CONFIGURING = {}
 _LOGGER = logging.getLogger(__name__)
 
+REQUIREMENTS = ['pymyair==0.1.0']
+
 DOMAIN = 'myair'
 
 DATA_MYAIR = 'myair'
 
 CONF_IP = 'ip'
 CONF_PORT = 'port'
-CONF_KEY = 'key'
-CONF_NAME = 'name'
+
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_IP): cv.string,
-        vol.Required(CONF_PORT): vol.All(vol.Coerce(int), vol.Range(min=1)),,
-        vol.Required(CONF_NAME): cv.string,
-        vol.Required(CONF_KEY): cv.string
+        vol.Required(CONF_PORT, default=2025): vol.All(vol.Coerce(int), vol.Range(min=1))
     })
-})
+}, extra=vol.ALLOW_EXTRA)
 
 def setup(hass, config):
-    """Setup the Myair component."""
-
-    import myair
-
-    conf = config[DOMAIN]
-
-    _LOGGER.debug("proceeding with setup")
-
-    conf = config[DOMAIN]
-    conf_ip = conf[CONF_IP]
-    conf_port = conf[CONF_PORT]
-    conf_key = conf[CONF_KEY]
-
-    myair = myair.MyAir(conf_ip,conf_port,conf_key)
-    
-    hass.data[DATA_MYAIR] = MyAirDevice(hass, conf, myair)
-    
     _LOGGER.debug("proceeding with discovery")
-    discovery.load_platform(hass, 'climate', DOMAIN, {}, config)
+    for platform in ['climate', 'sensor']:
+        discovery.load_platform(hass, platform, DOMAIN, {CONF_IP: config[DOMAIN].get(CONF_IP), CONF_PORT: config[DOMAIN].get(CONF_PORT)}, config)
     _LOGGER.debug("setup done")
 
     return True
-
-
-class MyAirDevice(object):
-    """Structure MyAir functions for hass."""
-
-    def __init__(self, hass, conf, myair):
-        """Init MyAir Devices."""
-        self.hass = hass
-        self._myair = myair
-
-    def zones(self):
-        """Generator returning list of devices and their location."""
-        try:
-            zonecount = self._myair.getZoneCount()
-            for zoneid in range(1,zonecount+1):
-                yield zoneid
-        except socket.error:
-            _LOGGER.error(
-                "Connection error connecting to the myair system")
-
-    @property
-    def myair(self):
-        return self._myair
-    
-
 
